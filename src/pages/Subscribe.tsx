24ad3cdab0c29d606f53841946bf0b49045c3f79
RodgoDeOliveiraSilva <rodrigodeoliveirasilva20@gmail.com>
2022-06-26 18:32:02 -0300

    removendo animacao do Subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -78,7 +78,7 @@ export function Subscribe() {
                 
             </div>
 
-            <div className="  display: flex w-full animate-rotagem gp-6 space-x-16">
+            <div className="  display: flex w-full  gp-6 space-x-16">
                 
                 <MicrosoftExcelLogo size={`20%`} color='blue' />
                 
